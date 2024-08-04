43ec3efb345a97afcd3c584d0e5acd01ba62f037
Leo Porto <leonardoreisporto@gmail.com>
2024-08-04 20:47:02 -0300

    Configurando TypeScript para o Projeto de IA com NodeJS

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
 async function main() {
     const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: "Crie uma lista dos 10 países mais ricos do mundo." }],
+        messages: [{ role: "system", content: "Crie uma lista dos 10 animes mais assistidos do mundo." }],
         model: "gpt-4o-mini",
     });
 
